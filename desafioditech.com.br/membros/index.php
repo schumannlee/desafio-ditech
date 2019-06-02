fff29c0da5f6cf829f407a265cf0517ef6f1eb14
unknown <verti.schumann@gmail.com>
2019-06-02 14:04:58 -0300

    Ajustes em topo e index

--- desafioditech.com.br/membros/index.php
+++ desafioditech.com.br/membros/index.php
@@ -1,21 +1,42 @@
 <?php
-	session_start();
+	include "topo.php";
 
+// VERIFICA SE HÁ SESSÃO VÁLIDA INICIADA
 	if (
 		!isset($_SESSION['logado']) || 
 		!isset($_SESSION['status_id']) || 
 		!isset($_SESSION['permissao'])
 	) {
 		session_unset();
-		include "../funcoes-php/config.php";
 		echo "<script>location.href='$url_principal';</script>";
 	}
+// PERMITE ACESSO À PÁGINA
 	else {
+?>
 
-		echo "<pre>";
+	<div class="titulo-pagina">
+		Controle de Uso das<br>Salas de Reunião
+	</div>
+
+	<div class="listagem">
+		<div class="listagem-titulo">
+			Lista de Salas para Reserva
+		</div>
+		<div class="listagem-exibicao">
+			Nenhuma disponível no momento.
+		</div>
+		
+	</div>
+
+<?php
+
+		echo "<br><br><pre>";
 		print_r($_SESSION);
 		echo "</pre>";
 
 		echo "<br><a href='sair.php'>SAIR</a>";
+
+		include "../rodape.php";
+
 	}
 ?>
---
+++ desafioditech.com.br/membros/menu.php
@@ -0,0 +1,11 @@
+<div id="menu">
+	<span>MENU</span>
+</div>
+<nav>
+	<a href="index.php">
+		<div>Acessar</div></a>
+	<a href="cadastro.php">
+		<div>Cadastrar</div></a>
+	<a href="contato.php">
+		<div>Contato</div></a>
+</nav>
